154ec71f87edbdad22615d933aef16b2feb36060
Loay Mohamed <72279810+zoroxide@users.noreply.github.com>
2024-02-02 16:24:27 +0200

    Stable V1.0.0

---
+++ main.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+from views import *
+
+SERVER_HOST = get_ip_address()
+SERVER_PORT = 8000
+
+def main():
+    # Define the layout
+    layout = [
+        [sg.Text("Minimalist HTTP Server With @PySimpleGUI", font=("Helvetica", 20), justification='center')],
+        [sg.Text("Select a File:", size=(15, 1)), sg.InputText(key="file_path"), sg.FileBrowse()],
+        [sg.Button("Start", size=(10, 1)), sg.Button("Shutdown", size=(10, 1))]
+    ]
+
+    # Create the window
+    window = sg.Window("HTTP SERVER", layout, resizable=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED:
+            break
+        elif event == "Start":
+            file_path = values["file_path"]
+            sg.popup(f"Server will Start and Listen in http://{SERVER_HOST}:{SERVER_PORT}", title="START SERVER ALERT")
+            start_server(file_path, SERVER_HOST,SERVER_PORT)
+        elif event == "Shutdown":
+            shutdown_server(SERVER_HOST, SERVER_PORT)
+
+
+    window.close()
+
+if __name__ == "__main__":
+    main()
---
+++ views.py
@@ -0,0 +1,52 @@
+from http.server import SimpleHTTPRequestHandler
+from socketserver import TCPServer
+import os, socket
+
+
+
+def get_ip_address():
+    try:
+        # Create a socket object
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Try connecting to a public server (Google's DNS server)
+        s.connect(("8.8.8.8", 80))
+
+        # Get the local IP address of the computer
+        ip_address = s.getsockname()[0]
+    except Exception as e:
+        print(f"Error Getting IPv4 address of the machine: {e}")
+        ip_address = None
+    finally:
+        # Close the socket
+        s.close()
+
+        return ip_address
+
+
+def start_server(htmlpath, host, port):
+    try:
+        # Get the directory of the HTML file
+        filepath = os.path.dirname(os.path.realpath(htmlpath))
+
+        # Change the current working directory to the HTML file directory
+        os.chdir(filepath)
+
+        # Create the HTTP server
+        server_address = (host, port)
+        httpd = TCPServer(server_address, SimpleHTTPRequestHandler)
+        print(f"Server running at http://{host}:{port}")
+
+        # Start the server
+        httpd.serve_forever()
+    except Exception as e:
+        print(f"Error Starting Server: {e}")
+
+
+def shutdown_server(host, port):
+    try:
+        server_address = (host, port)
+        httpd = TCPServer(server_address, SimpleHTTPRequestHandler)
+        httpd.shutdown()
+    except Exception as e:
+        print(f"Error Shutting Down Server: {e}")
